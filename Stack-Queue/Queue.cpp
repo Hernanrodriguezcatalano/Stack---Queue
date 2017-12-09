0bf326cd9da8ea95ed279d638899230823750648
Hernanrodriguezcatalano <hernanrodriguezcatalano@gmail.com>
2017-12-09 10:59:07 -0300

    Add files via upload

---
+++ Stack-Queue/Queue.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<array>
+
+const unsigned N=4;
+struct Queue {
+    std::array <int,N> a;
+    unsigned front=0,rear=0, size_t=0;
+};
+
+void Enqueue (Queue&, int);
+int Dequeue (Queue&);
+
+int main() {
+    Queue q;
+    Enqueue (q,1);
+    Enqueue (q,2);
+    Enqueue (q,3);
+    std::cout<< Dequeue(q) << "\n";
+    Enqueue (q,4);
+    Enqueue (q,5);
+    std::cout<< "El tamaño de la pila es : " << q.size_t << "\n";
+}
+
+void Enqueue (Queue& q, int x){
+    q.a.at(q.rear)=x;
+    q.rear=(q.rear+1) % N;
+    ++q.size_t;
+}
+
+int Dequeue (Queue& q){
+    --q.size_t;
+    q.front=(q.front+1) %N;
+    return q.a.at(q.front-1);
+}
+
---
+++ Stack-Queue/Stack.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <array>
+
+
+const unsigned N=800;
+struct Stack {
+    std::array <int,N> a;
+    unsigned i=0;
+};
+
+void Push (Stack&, int);
+int Pop (Stack&);
+
+int main() {
+    Stack s;
+    Push (s,42);
+    std::cout<< Pop(s);
+}
+
+void Push (Stack& s, int x){
+    s.a.at(s.i)=x;
+    ++s.i;
+}
+
+int Pop (Stack& s){
+    --s.i;
+    return s.a.at(s.i);
+}
